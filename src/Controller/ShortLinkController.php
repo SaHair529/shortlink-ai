c7cb9b738fea79d11e6801ac0ffe2feee74e693b
SaHair529 <sa.hairulaev@gmail.com>
2025-06-08 14:07:32 +0300

    Создание короткой ссылки с хешем сделано

--- src/Controller/ShortLinkController.php
+++ src/Controller/ShortLinkController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\ShortLink;
 use App\Service\LinkNormalizer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,12 +14,12 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ShortLinkController extends AbstractController
 {
-    public function __construct(private TranslatorInterface $translator)
+    public function __construct(private TranslatorInterface $translator, private EntityManagerInterface $em)
     {
         
     }
 
-    #[Route('/', methods: ['POST'])]
+    #[Route('/', methods: ['GET'])]
     public function create(Request $request): JsonResponse
     {
         $link = $request->query->get('link');
@@ -32,9 +34,25 @@ class ShortLinkController extends AbstractController
         if (!$normalizedLink) {
             return $this->json([
                 'error' => $this->translator->trans('request.shortlink.create.invalid_link_param')
-            ]);
+            ], Response::HTTP_BAD_REQUEST);
         }
 
-        return $this->json(['message' => 'ok']);
+        $shortLinkEntity = $this->em->getRepository(ShortLink::class)->findOneBy(['originalLink' => $link]) ?? new ShortLink();
+        if ($shortLinkEntity->getId()) {
+            return $this->json($shortLinkEntity, Response::HTTP_OK, [], ['groups' => ['shortLink']]);
+        }
+
+        $shortLinkEntity->setShortLink($this->generateRandomShortPath(32));
+        $shortLinkEntity->setOriginalLink($link);
+
+        $this->em->persist($shortLinkEntity);
+        $this->em->flush();
+
+        return $this->json($shortLinkEntity, Response::HTTP_OK, [], ['groups' => ['shortLink']]);
+    }
+
+    private function generateRandomShortPath(int $length = 6): string
+    {
+        return substr(bin2hex(random_bytes($length)), 0, $length);
     }
 }
---
+++ src/Entity/ShortLink.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ShortLinkRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
+
+#[ORM\Entity(repositoryClass: ShortLinkRepository::class)]
+class ShortLink
+{
+    #[Groups(['shortLink'])]
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[Groups(['shortLink'])]
+    #[ORM\Column(length: 255)]
+    private ?string $shortLink = null;
+
+    #[Groups(['shortLink'])]
+    #[ORM\Column(length: 255)]
+    private ?string $originalLink = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getShortLink(): ?string
+    {
+        return $this->shortLink;
+    }
+
+    public function setShortLink(string $shortLink): static
+    {
+        $this->shortLink = $shortLink;
+
+        return $this;
+    }
+
+    public function getOriginalLink(): ?string
+    {
+        return $this->originalLink;
+    }
+
+    public function setOriginalLink(string $originalLink): static
+    {
+        $this->originalLink = $originalLink;
+
+        return $this;
+    }
+}
