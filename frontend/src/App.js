3ca3c18e7fec10bd8d3124ac7cd5a62cefcc97f8
SaHair529 <sa.hairulaev@gmail.com>
2025-06-12 21:06:25 +0300

    Добавить контейнер Mantine вокруг маршрутов в компоненте App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import { Container } from '@mantine/core';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Container px="sm" size="xs">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Container>
+
     </BrowserRouter>
   );
 }
